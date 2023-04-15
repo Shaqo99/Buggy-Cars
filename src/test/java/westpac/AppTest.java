97bb661c9cf5406a121189726af4462189287110
Shaqo99 <52594895+Shaqo99@users.noreply.github.com>
2023-04-15 15:30:48 +1200

    first commit

---
+++ src/test/java/westpac/AppTest.java
@@ -0,0 +1,20 @@
+package westpac;
+
+import org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
