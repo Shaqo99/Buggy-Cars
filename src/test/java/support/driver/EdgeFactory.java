ac0b009e627f1bfbea8216cabc14166e9ded1dab
Shaqo99 <52594895+Shaqo99@users.noreply.github.com>
2023-04-16 19:51:27 +1200

    added edge driver

---
+++ src/test/java/support/driver/EdgeFactory.java
@@ -0,0 +1,23 @@
+package support.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
+public class EdgeFactory extends DriverFactory{
+
+    @Override
+    public WebDriver getDriver() {
+        return new EdgeDriver(getCapabilities());
+    }
+
+    @Override
+    public EdgeOptions getCapabilities() {
+        var options = new EdgeOptions();
+        options.addArguments("--disable-gpu",
+                "--window-size=1920,1200",
+                "--ignore-certificate-errors");
+        return options;
+    }
+    
+}
